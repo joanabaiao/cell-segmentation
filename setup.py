9f861b0b5af5dec98154e85015704b9bd3e92639
joanabaiao <joanabaiao99@gmail.com>
2024-06-28 11:28:11 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Joana',
+    author_email= 'joanabaiao25@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
